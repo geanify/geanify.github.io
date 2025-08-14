617ff1c0e0f21614b65773b9b9fefd5f47c350bb
geanify <geanify@gmail.com>
2025-08-14 11:27:19 +0300

    presentation herald

--- dev-server.ts
+++
@@ -1,5 +0,0 @@
-import 'dotenv/config';
-process.env.NODE_ENV = 'development';
-import { createWebHeraldServer } from "./index";
-
-createWebHeraldServer({ port: 3000 }); 
--- prod-server.ts
+++
@@ -1,8 +0,0 @@
-import { createWebHeraldServer } from "./index";
-
-// TODO: Implement no-ip update logic here
-// Example: await updateNoIp({ ... });
-
-process.env.NODE_ENV = 'production';
-
-createWebHeraldServer({ port: 3000 }); 
